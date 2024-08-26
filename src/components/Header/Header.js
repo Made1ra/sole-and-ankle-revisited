809fdc5e61391f7c30bd3112c71931cefedd8239
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-08-26 10:54:19 +0300

    feat: add mobile header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,9 @@ import styled from "styled-components/macro";
 
 import Logo from "../Logo";
 import SuperHeader from "../SuperHeader";
+import Icon from "../Icon";
 import MobileMenu from "../MobileMenu";
+import { QUERIES } from "../../constants";
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -12,6 +14,7 @@ const Header = () => {
   // with an onClick handler, something like this:
   //
   // <button onClick={() => setShowMobileMenu(true)}>
+  const isSmall = window.matchMedia(QUERIES.tabletAndDown).matches;
 
   return (
     <header>
@@ -20,15 +23,29 @@ const Header = () => {
         <Side>
           <Logo />
         </Side>
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side />
+        {isSmall ? (
+          <Icons>
+            <Icon id="shopping-bag" strokeWidth={2} />
+            <Icon id="search" strokeWidth={2} />
+            <Icon
+              id="menu"
+              strokeWidth={2}
+              onClick={() => setShowMobileMenu(true)}
+            />
+          </Icons>
+        ) : (
+          <>
+            <Nav>
+              <NavLink href="/sale">Sale</NavLink>
+              <NavLink href="/new">New&nbsp;Releases</NavLink>
+              <NavLink href="/men">Men</NavLink>
+              <NavLink href="/women">Women</NavLink>
+              <NavLink href="/kids">Kids</NavLink>
+              <NavLink href="/collections">Collections</NavLink>
+            </Nav>
+            <Side />
+          </>
+        )}
       </MainHeader>
 
       <MobileMenu
@@ -45,6 +62,10 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid var(--color-gray-300);
+
+  @media ${QUERIES.mobile} {
+    padding-inline: 16px;
+  }
 `;
 
 const Nav = styled.nav`
@@ -55,6 +76,14 @@ const Nav = styled.nav`
 
 const Side = styled.div`
   flex: 1;
+`;
+
+const Icons = styled.div`
+  height: 1.5rem;
+  flex-shrink: 0;
+  display: flex;
+  align-self: center;
+  gap: 2rem;
 `;
 
 const NavLink = styled.a`
