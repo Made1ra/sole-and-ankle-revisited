6d17019a82badb8900924b1b232261c42a412ba1
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-08-27 12:32:21 +0300

    feat: add mobile menu

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,35 +1,116 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components/macro';
-import { DialogOverlay, DialogContent } from '@reach/dialog';
+import React from "react";
+import styled from "styled-components/macro";
+import { DialogOverlay, DialogContent } from "@reach/dialog";
 
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
-  if (!isOpen) {
-    return null;
-  }
-
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <>
+      {isOpen && <Background onClick={onDismiss} />}
+      <Overlay isOpen={isOpen} onDismiss={onDismiss}>
+        <Content aria-label="Navigation menu">
+          <StyledButton onClick={onDismiss}>
+            <Icon id="close" strokeWidth={2} />
+            <VisuallyHidden>Dismiss modal</VisuallyHidden>
+          </StyledButton>
+          <Nav>
+            <NavLink href="/sale">Sale</NavLink>
+            <NavLink href="/new">New&nbsp;Releases</NavLink>
+            <NavLink href="/men">Men</NavLink>
+            <NavLink href="/women">Women</NavLink>
+            <NavLink href="/kids">Kids</NavLink>
+            <NavLink href="/collections">Collections</NavLink>
+          </Nav>
+          <FooterNav>
+            <FooterNavLink href="/terms">Terms and Conditions</FooterNavLink>
+            <FooterNavLink href="/privacy">Privacy Policy</FooterNavLink>
+            <FooterNavLink href="/contact">Contact Us</FooterNavLink>
+          </FooterNav>
+        </Content>
+      </Overlay>
+    </>
   );
 };
+
+const Background = styled.div`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(96, 100, 108, 0.8);
+  z-index: 1;
+`;
+
+const Overlay = styled(DialogOverlay)`
+  position: fixed;
+  top: 0;
+  right: 0;
+  background: var(--color-white);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  height: 100%;
+`;
+
+const Content = styled(DialogContent)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 11.25rem;
+  position: relative;
+  background: var(--color-white);
+  width: 100%;
+  height: 100%;
+`;
+
+const StyledButton = styled(UnstyledButton)`
+  position: absolute;
+  top: 2rem;
+  right: 1rem;
+  width: 1.5rem;
+  height: 1.5rem;
+  flex-shrink: 0;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 1.38rem;
+  margin: 0 2rem;
+`;
+
+const NavLink = styled.a`
+  color: var(--color-gray-900);
+  font-family: Raleway;
+  font-size: 1.125rem;
+  font-style: normal;
+  font-weight: var(--font-weight-medium);
+  line-height: normal;
+  text-transform: uppercase;
+  text-decoration: none;
+
+  &:first-of-type {
+    color: var(--color-secondary);
+  }
+`;
+
+const FooterNav = styled.footer`
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+  gap: 0.88rem;
+  height: 4.75rem;
+  margin: 2rem;
+`;
+
+const FooterNavLink = styled.a`
+  text-decoration: none;
+  color: var(--color-gray-700);
+`;
 
 export default MobileMenu;
